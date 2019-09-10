80be0477814d50bb650b4cfedd4388cf8f4626c3
Jamon Holmgren <jamon@infinite.red>
2019-09-10 20:06:20 +0200

    Initial commit Ignite CLI version 3.1.6

---
+++ app/navigation/primary-navigator.ts
@@ -0,0 +1,22 @@
+import { createStackNavigator } from "react-navigation"
+import { WelcomeScreen } from "../screens/welcome-screen"
+import { DemoScreen } from "../screens/demo-screen"
+
+export const PrimaryNavigator = createStackNavigator(
+  {
+    welcome: { screen: WelcomeScreen },
+    demo: { screen: DemoScreen },
+  },
+  {
+    headerMode: "none",
+  },
+)
+
+/**
+ * A list of routes from which we're allowed to leave the app when
+ * the user presses the back button on Android.
+ *
+ * Anything not on this list will be a standard `back` action in
+ * react-navigation.
+ */
+export const exitRoutes: string[] = ["welcome"]
